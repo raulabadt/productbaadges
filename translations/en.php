2106747633b0feaf4b231c35869a167db824cb0e
Raul Abad <raulabadtorralba@gmail.com>
2026-05-27 18:10:00 +0200

    feat: añadir traducciones del módulo

---
+++ translations/en.php
@@ -0,0 +1,5 @@
+<?php
+$translations = [
+    'Product Badges' => 'Product Badges',
+    'Manage reusable visual badges for products.' => 'Manage reusable visual badges for products.',
+];
---
+++ translations/es.php
@@ -0,0 +1,5 @@
+<?php
+$translations = [
+    'Product Badges' => 'Badges de Producto',
+    'Manage reusable visual badges for products.' => 'Gestiona etiquetas visuales reutilizables para productos.',
+];
